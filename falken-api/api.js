cf9c657bdeaa4cbf9f934b5d3998a4666431f8a4
Mike Goddard <mikeygiom@gmail.com>
2020-11-02 22:41:40 +0000

    Improved readme, added HTTP score modification endpoint

--- falken-api/api.js
+++ falken-api/api.js
@@ -2,16 +2,38 @@ const serverless = require('serverless-http');
 
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser')
+const AWS = require('aws-sdk');
 
 const app = express();
 const port = 3000;
 const { getAllScores } = require('./data');
 
+AWS.config.update({region: 'eu-west-2'});
+const sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get('/scores', async (req, res) => {
   const scores = await getAllScores();
   res.json(scores);
+});
+
+app.put('/scores', async (req, res) => {
+  console.info(JSON.stringify(req.body));
+  const params = {
+    MessageBody: JSON.stringify(req.body),
+    QueueUrl: process.env.QUEUE_URL
+  };
+  try {
+    const send_result = await sqs.sendMessage(params).promise();
+    console.info("Sent to SQS: " + send_result);
+    res.send(200);
+  } catch (error) {
+    console.error("Error sending to SQS: " + error);
+    res.send(500);
+  }
 });
 
 app.use((req, res) => {
@@ -20,7 +42,7 @@ app.use((req, res) => {
 
 if (process.env.NODE_ENV !== 'test') {
   app.listen(port, () => {
-    console.log(`falken-api listening at http://localhost:${port}`);
+    console.info(`falken-api listening at http://localhost:${port}`);
   });
 }
 
